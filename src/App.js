4a1692334b64b4ab7e741cbcf4ff688d74614ade
Jay Liang <jayliang789@gmail.com>
2024-09-28 13:19:32 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import dexscreener from "../src/img/dexscreener.png"
+import telegram from "../src/img/telegram.png"
+import twitter from "../src/img/twitter.png"
+import dextools from "../src/img/dextools.png"
+import background from "../src/img/353kaymar.png"
 import './App.css';
+function copyText() {
+  const textToCopy = document.getElementById("textToCopy").innerText;
 
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      // Optional: Display a success message
+      alert("Text copied to clipboard!");
+    })
+    .catch(err => {
+      console.error("Failed to copy: ", err);
+    });
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`, 
+        backgroundSize: 'cover',   // Make sure the background covers the entire area
+        backgroundPosition: 'center', // Center the image
+        backgroundRepeat: 'no-repeat', // Prevent the image from repeating
+        minHeight: '100vh', // Ensure the div takes up the full viewport height
+      }}
+    >
+        <header className="Meme-Intro">
+          <p style={{fontWeight:'bold', fontSize:'50px', color:'white'}}>
+            FUCK 350
+          </p>
+          <div style={{marginBottom:'5vh'}}>
+            <button className='CA' id="textToCopy" onClick={copyText}>CA:FK350 </button>
+          </div>
+          <div style={{display: 'flex'}}>
+            <a href="https://x.com" target="_blank" rel="noreferrer">
+              <button className='Socials' style={{ marginRight:'2vh'}}>
+                <span>
+                  <img src={process.env.PUBLIC_URL + "/img/telegram.png"} alt="telegram" style={{width: 'auto', height:'auto'}}/>
+                </span>
+              </button>
+            </a>{" "}  
+            <a href="https://dexscreener.com" target="_blank" rel="noreferrer">
+              <button className='Socials' style={{ marginRight:'2vh'}}>
+                <span>
+                  <img src={process.env.PUBLIC_URL + "/img/dexscreener.png"} alt="Dexscreener" style={{width: 'auto',height:'auto'}}/>
+                </span>
+              </button>
+            </a>
+            <a href="https://x.com" target="_blank" rel="noreferrer">
+              <button className='Socials' style={{ marginRight:'2vh'}}>
+                <span>
+                  <img src={process.env.PUBLIC_URL + "/img/twitter.png"} alt="twitter" style={{width: 'auto',height:'auto'}}></img>
+                </span>
+              </button>
+            </a>
+            <a href="https://dextools.io" target="_blank" rel="noreferrer">
+              <button className='Socials'>
+                <span>
+                  <img src={process.env.PUBLIC_URL + "/img/dextools.png"} alt="dextools" style={{width: 'auto',height:'auto'}}></img>
+                </span>
+              </button>
+            </a>
+          </div>
+        </header>
     </div>
   );
 }
+
 
 export default App;
